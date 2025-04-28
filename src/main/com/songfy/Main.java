cbd57f0314a71664d7f9233bd1228b3b8e021859
songfy <1146583409@qq.com>
2025-04-28 11:39:52 +0800

    fix the data_dir create bug

--- src/main/com/songfy/Main.java
+++ src/main/com/songfy/Main.java
@@ -4,10 +4,21 @@ import lombok.extern.slf4j.Slf4j;
 import main.com.songfy.ui.GoldTradingApp;
 
 import java.awt.*;
+import java.io.File;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
+        // 在项目路径创建文件夹 data
+        File dataDir = new File("./data");
+        if (!dataDir.exists()) {
+            boolean created = dataDir.mkdirs();
+            if (created) {
+                log.info("文件夹 data 创建成功");
+            } else {
+                log.error("文件夹 data 创建失败");
+            }
+        }
         EventQueue.invokeLater(GoldTradingApp::new);
     }
 }
